3fa55feea932122ccb9a2b56f476f5eed474566a
Gearz <shanegeary1994@gmail.com>
2021-09-18 09:31:35 -0600

    Added ContactList function component

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -5,15 +5,19 @@ class AddContact extends React.Component {
         return (
             <div className="ui main">
                 <h2>Add Contact</h2>
+                <br />
                 <form className="ui form">
                     <div className="field">
                         <label>Name</label>
                         <input type="text" name="name" placeholder="Name" />
                     </div>
+                    <br />
                     <div className="field">
                         <label>Email</label>
                         <input type="text" name="email" placeholder="Email" />
                     </div>
+                    <br />
+                    <button className="ui button green">Create</button>
                 </form>
             </div>
         );
