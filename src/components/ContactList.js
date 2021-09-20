f3a866a9f7314b465fb4e2a32042d67ee6d77c15
Gearz <shanegeary1994@gmail.com>
2021-09-20 11:59:39 -0600

    renderContactList in ContactList component

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 const ContactList = (props) => {
     console.log(props);
+
+    const renderContactList = props.contacts.map((contact) => {
+        return (
+            <div className="item">
+                <div className="content">
+                    <div className="header">{contact.name}</div>
+                    <div>{contact.email}</div>
+                </div>
+            </div>
+        );
+    })
     return (
-        <div className="ui celled list">Contact List</div>
+        <div className="ui celled list">
+            {renderContactList}
+        </div>
     );
 };
 
