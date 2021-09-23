4083007ab9c8f0ffce6062f118f7c6520c7550a5
Gearz <shanegeary1994@gmail.com>
2021-09-23 09:36:48 -0600

    Passed props from ContactList to ContactCard

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const ContactCard = () => {
+const ContactCard = (props) => {
+    const { id, name, email } = props.contact;
+
     return (
         <div className="item">
             <div className="content">
-                <div className="header">{contact.name}</div>
-                <div>{contact.email}</div>
+                <div className="header">{name}</div>
+                <div>{email}</div>
             </div>
         </div>
     );
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,16 +1,12 @@
 import React from 'react';
+import ContactCard from './ContactCard';
 
 const ContactList = (props) => {
     console.log(props);
 
     const renderContactList = props.contacts.map((contact) => {
         return (
-            <div className="item">
-                <div className="content">
-                    <div className="header">{contact.name}</div>
-                    <div>{contact.email}</div>
-                </div>
-            </div>
+            <ContactCard contact={contact} />
         );
     })
     return (
