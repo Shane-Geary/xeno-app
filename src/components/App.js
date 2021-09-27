8dded4581e602147b0d7316c08b6ae0ecaebc07b
Gearz <shanegeary1994@gmail.com>
2021-09-27 11:34:29 -0600

    Added useEffect method imported from React to save newly created contacts to localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
@@ -7,10 +7,16 @@ import ContactList from './ContactList';
 function App() {
   const [contacts, setContacts] = useState([]);
 
+  const LOCAL_STORAGE_KEY = "contacts";
+
   const addContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, contact]);
   }
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
    <div>
